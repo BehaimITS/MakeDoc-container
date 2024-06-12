d5d178f9aad9c127b858309bd835b57472330d0a
lmajer@behaimits.com <144108104+lmajer999@users.noreply.github.com>
2024-06-12 22:15:22 +0200

    Update transform.sh

--- makedoc/transform.sh
+++ makedoc/transform.sh
@@ -7,9 +7,15 @@ output_path=${2:-mkdocs.yml}
 # Function to transform a line
 transform_line() {
     local line=$1
-    line=$(echo $line | sed 's/^- *//')         # Remove leading list markers
-    line=$(echo $line | sed 's/\[\(.*\)\](\(.*\))/\1: \2/')  # Replace [text](link) with text: link
-    echo $line
+    line=$(echo "$line" | sed 's/^- *//')  # Remove leading list markers
+    line=$(echo "$line" | sed 's/\[\(.*\)\](\(.*\))/\1: \2/')  # Replace [text](link) with text: link
+    echo "$line"
+}
+
+# Function to calculate indent length
+calculate_indent_length() {
+    local line="$1"
+    echo $(echo "$line" | sed -e 's/[^ \t].*//' | wc -c)
 }
 
 # Read input file and transform lines
@@ -20,17 +26,47 @@ transform_line() {
     echo '# docs_dir: ./'
     echo ''
     echo 'nav:'
-    
-    while IFS= read -r line; do
-        if [[ -n "$line" ]]; then
-            indent_count=$(echo "$line" | sed -e 's/[^ ]//g' | wc -c)
-            indent=""
-            for (( i=2; i<indent_count; i+=4 )); do
-                indent="$indent  "
-            done
-            echo "${indent}- $(transform_line "$line")"
+
+    # Initialize previous_line and previous_indent_length
+    previous_line=""
+    previous_indent_length=0
+
+    while IFS= read -r current_line || [ -n "$current_line" ]; do
+        if [[ -n "$current_line" && ! "$current_line" =~ ^# ]]; then
+            current_indent_length=$(calculate_indent_length "$current_line")
+
+            # Check if previous_line exists to process it
+            if [[ -n "$previous_line" ]]; then
+                indent=""
+                for (( i=2; i<previous_indent_length; i+=4 )); do
+                    indent="$indent  "
+                done
+
+                if [[ $previous_indent_length -lt $current_indent_length ]]; then
+                    transformed_line=$(echo "$previous_line" | sed 's/\[\(.*\)\](\(.*\))/\1:/')
+                else
+                    transformed_line=$(transform_line "$previous_line")
+                fi
+
+                echo "${indent}$transformed_line"
+            fi
+
+            # Update previous_line and previous_indent_length
+            previous_line="$current_line"
+            previous_indent_length="$current_indent_length"
         fi
     done < "$input_path"
+
+    # Handle the last line if any
+    if [[ -n "$previous_line" && ! "$previous_line" =~ ^# ]]; then
+        indent=""
+        for (( i=2; i<previous_indent_length; i+=4 )); do
+            indent="$indent  "
+        done
+
+        transformed_line=$(transform_line "$previous_line")
+        echo "${indent}$transformed_line"
+    fi
 } > "$output_path"
 
 echo "File transformed and saved to $output_path"
