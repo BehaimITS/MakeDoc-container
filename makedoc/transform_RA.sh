e2c8e468f46b32ecc6b079756a5b4fb461ae557a
lmajer@behaimits.com <144108104+lmajer999@users.noreply.github.com>
2024-07-11 10:11:18 +0200

    Update transform_RA.sh

--- makedoc/transform_RA.sh
+++ makedoc/transform_RA.sh
@@ -8,18 +8,30 @@ output_path=${2:-mkdocs.yml}
 transform_line() {
     local line=$1
     line=$(echo "$line" | sed -E 's|\[([^]]*/)?([^]/]+)\](\([^)]*\))|\[\2\]\3|; s|\[\](\([^)]*\))|\[default\](\1)|')
-    line=$(echo "$line" | sed 's/\[\(.*\)\](\(.*\))/\1: \/ra\/\2/')  # Nahraď [text](link) text: /ra/link
+    line=$(echo "$line" | sed 's/\[\(.*\)\](\(.*\))/\1: \/ra\/\2/')
     echo "$line"
 }
 
 # Funkce pro výpočet délky odsazení
 calculate_indent_length() {
     local line="$1"
-    echo $(echo "$line" | sed -e 's/[^ \t].*//' | wc -c)
+    local indent_length=$(echo "$line" | sed -e 's/[^ \t].*//' | wc -c)
+    echo $indent_length
 }
+
+# Smazání řádků za "Repository Analyst documentation:"
+if grep -q "Repository Analyst documentation:" "$output_path"; then
+    # Pomocí ed (textový editor) k odstranění řádků od "Repository Analyst documentation:" včetně
+    ed -s "$output_path" <<EOF
+/Repository Analyst documentation:/,\$d
+w
+EOF
+fi
 
 # Připojení transformovaných řádků do výstupního souboru
 {
+    echo '-   Repository Analyst documentation:'
+
     # Kontrola, zda výstupní soubor existuje a není prázdný
     if [[ ! -s "$output_path" ]]; then
         echo 'site_name: "bwce-integration-flow"'
@@ -30,71 +42,33 @@ calculate_indent_length() {
         echo 'nav:'
     fi
 
-    # Inicializace previous_line a previous_indent_length
-    previous_line=""
     previous_indent_length=0
-
-    first_line=true
 
     while IFS= read -r current_line || [ -n "$current_line" ]; do
         if [[ -n "$current_line" && ! "$current_line" =~ ^# ]]; then
             current_indent_length=$(calculate_indent_length "$current_line")
 
-            # Zkontrolujte, zda existuje previous_line pro jeho zpracování
-            if [[ -n "$previous_line" ]]; then
-                indent=""
-                for (( i=2; i<previous_indent_length; i+=4 )); do
-                    indent="$indent  "
-                done
+            # Odsazení
+            indent=""
+            for (( i=2; i<current_indent_length; i+=4 )); do
+                indent="$indent  "
+            done
 
-                if [[ $previous_indent_length -lt $current_indent_length ]]; then
-                    transformed_line=$(echo "$previous_line" | sed 's/\[\(.*\)\](\(.*\))/\1:/')
-                else
-                    transformed_line=$(transform_line "$previous_line")
-                fi
+            # Transformace řádku
+            transformed_line=$(transform_line "$current_line")
 
-                # Zkontrolujte, zda je to první řádek, který připojujete
-                if $first_line; then
-                    echo ""  # Přidání prázdného řádku před prvním připojeným řádkem
-                    transformed_line=$(echo "$transformed_line" | sed 's/Introduction:/Repository Analyst documentation:/')
-                    transformed_line=$(echo "$transformed_line" | sed 's/: \/ra\/intro.md/:/')
-                    first_line=false
-                else
-                    # Odsazení ostatních řádků
-                    indent="      $indent"
-                fi
-
-                echo "${indent}$transformed_line"
+            # Výstup transformovaného řádku
+            if [[ $previous_indent_length -lt $current_indent_length ]]; then
+                indent="      $indent"
+            else
+                indent="      $indent"
             fi
+            echo "${indent}$transformed_line"
 
-            # Aktualizace previous_line a previous_indent_length
-            previous_line="$current_line"
+            # Aktualizace previous_indent_length
             previous_indent_length="$current_indent_length"
         fi
     done < "$input_path"
-
-    # Zpracování posledního řádku, pokud existuje
-    if [[ -n "$previous_line" && ! "$previous_line" =~ ^# ]]; then
-        indent=""
-        for (( i=2; i<previous_indent_length; i+=4 )); do
-            indent="$indent  "
-        done
-
-        transformed_line=$(transform_line "$previous_line")
-
-        # Zkontrolujte, zda je to první řádek, který připojujete
-        if $first_line; then
-            echo ""  # Přidání prázdného řádku před prvním připojeným řádkem
-            transformed_line=$(echo "$transformed_line" | sed 's/Introduction:/Repository Analyst documentation:/')
-            transformed_line=$(echo "$transformed_line" | sed 's/: \/ra\/intro.md/:/')
-            first_line=false
-        else
-            # Odsazení ostatních řádků
-            indent="  $indent"
-        fi
-
-        echo "${indent}$transformed_line"
-    fi
 } >> "$output_path"
 
 echo "Soubor transformován a připojen k $output_path"
